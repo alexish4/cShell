c18d26e1915aa05d0a749d2721153a4d380bbf0f
Alex <alexish4@cpp.edu>
2021-12-10 20:14:33 +0000

    Highlight numbers

--- kilo.c
+++ kilo.c
@@ -204,15 +204,31 @@ int getWindowSize(int *rows, int * cols)
 
 //syntax higlighting
 
+int is_separator(int c) {
+  return isspace(c) || c == '\0' || strchr(",.()+-/*=~%<>[];", c) != NULL;
+}
+
 void editorUpdateSyntax(erow *row) {
   row->hl = realloc(row->hl, row->rsize);
   memset(row->hl, HL_NORMAL, row->rsize);
 
-  int i;
-  for (i = 0; i < row->rsize; i++) {
-    if (isdigit(row->render[i])) {
+  int prev_sep = 1;
+
+  int i = 0;
+  while (i < row->rsize) {
+    char c = row->render[i];
+    unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+
+    if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) ||
+      (c == '.' && prev_hl == HL_NUMBER)) {
       row->hl[i] = HL_NUMBER;
+      i++;
+      prev_sep = 0;
+      continue;
     }
+
+    prev_sep = is_separator(c);
+    i++;
   }
 }
 
@@ -556,6 +572,7 @@ void abFree(struct abuf *ab)
 {
   free(ab->b);
 }
+
 //output
 
 void editorScroll() {
